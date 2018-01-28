24c91bd644ca6c7dc33ed1389be44ca79b347d77
qin2 <qin2@qq.com>
2018-01-28 20:37:21 +0800

    chapter5 build ok !

---
+++ B04283_05_code/build/CMakeFiles/Chapter5.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chapter5.dir/main.cpp.o"
+  "CMakeFiles/Chapter5.dir/utils/MultipleImageWindow.cpp.o"
+  "Chapter5.pdb"
+  "Chapter5"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chapter5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
