79c850bce36f7fadbd3ae146c168214b0c4442bd
qin2 <qin2@qq.com>
2018-01-28 20:31:45 +0800

    add chapter4 build ok !

---
+++ B04283_04_code/build/CMakeFiles/Chapter4_Phototool.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chapter4_Phototool.dir/main.cpp.o"
+  "Chapter4_Phototool.pdb"
+  "Chapter4_Phototool"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chapter4_Phototool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
